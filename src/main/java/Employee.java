72f65b5516fe7e487bc40c6e2220bc2a97d1e776
eriinys <eriinys@duck.com>
2025-10-22 13:03:48 -0500

    added punchTimeCard method in Employee class

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,10 +1,25 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class Employee {
     private int employeeId;
     private String name;
     private String department;
     private double payRate;
     private double hoursWorked;
+    private boolean punchedIn;
 
+    public Employee(int employeeId, String name, String department, double payRate, double hoursWorked, boolean punchedIn) {
+        this.employeeId = employeeId;
+        this.name = name;
+        this.department = department;
+        this.payRate = payRate;
+        this.hoursWorked = hoursWorked;
+        this.punchedIn = punchedIn;
+    }
+
+    public Employee(){}
 
     public int getEmployeeId() {
         return employeeId;
@@ -65,5 +80,39 @@ public class Employee {
             overtimeHours = 40 - hoursWorked;
         }
         return overtimeHours;
+    }
+
+    public boolean isPunchedIn() {
+        return punchedIn;
+    }
+
+    public void setPunchedIn(boolean punchedIn) {
+        this.punchedIn = punchedIn;
+    }
+
+    public void punchTimeCard(boolean punchingIn, double punchTime) {
+
+        if (punchingIn == true && !punchedIn) {
+            punchedIn = true;
+            System.out.printf("Punched in successfully at %.2f%n%n", punchTime);
+        } else if (!punchingIn && punchedIn) {
+            punchedIn = false;
+            System.out.printf("Punched out successfully at %.2f%n%n", punchTime);
+        } else {
+            System.err.println("Invalid action.");
+        }
+    }
+
+    public void punchTimeCard(boolean punchingIn, LocalTime punchTime) {
+
+        if (punchingIn == true && !punchedIn) {
+            punchedIn = true;
+            System.out.printf("Punched in successfully at %.2f%n%n", punchTime);
+        } else if (!punchingIn && punchedIn) {
+            punchedIn = false;
+            System.out.printf("Punched out successfully at %.2f%n%n", punchTime);
+        } else {
+            System.err.println("Invalid action.");
+        }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,65 @@
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Room room;
-        Reservation reservation = new Reservation();
-        Employee employee;
-
         Scanner myScanner = new Scanner(System.in);
 
-        System.out.println("""
-                Pick a room type:
-                King
-                Double""");
-        String roomType = myScanner.nextLine();
-        reservation.setRoomType(roomType);
+        Room room;
+        Reservation reservation = new Reservation();
+        Employee employee = new Employee();
 
-        System.out.println("\nNumber of nights staying: ");
-        int numOfNights = Integer.parseInt(myScanner.next());
-        reservation.setNumberOfNights(numOfNights);
+        while (true) {
+            System.out.println("""
+                    Welcome to Hotel! 
+                    Please choose from the following option:
+                    A) Guest
+                    B) Employee""");
+            String choice = myScanner.nextLine().toUpperCase();
+            switch (choice) {
+                case "A" -> {
+                    System.out.println("""
+                            Pick a room type:
+                            King
+                            Double""");
 
-        System.out.printf("Total price: $%.2f", reservation.getReservationTotal());
+                    String roomType = myScanner.nextLine();
+                    reservation.setRoomType(roomType);
+
+                    System.out.println("\nNumber of nights staying: ");
+                    int numOfNights = Integer.parseInt(myScanner.next());
+                    reservation.setNumberOfNights(numOfNights);
+
+                    System.out.printf("Total price: $%.2f", reservation.getReservationTotal());
+                }
+                case "B" -> {
+                    System.out.println("""
+                            Choose from following option:
+                            I) Puching In
+                            O) Punchin Out
+                            """);
+                    String option = myScanner.nextLine().toUpperCase();
+                    switch (option){
+                        case "I" -> {
+                            LocalTime timeNow = LocalTime.now();
+                            DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+                            double time = Double.parseDouble(timeNow.format(timeFormatter).toString().replace(":" , "."));
+                            employee.punchTimeCard(true, time);
+                        }
+                        case "O" -> {
+                            LocalTime timeNow = LocalTime.now();
+                            DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+                            double time = Double.parseDouble(timeNow.format(timeFormatter).toString().replace(":" , "."));
+                            employee.punchTimeCard(false, time);
+                        }
+                    }
+
+                }
+            }
+        }
 
     }
 }
